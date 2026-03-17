3e924887c1fa1b01ad25b1de19e569d74b02be1d
sonaligupta9079-del <sonaligupta9079@gmail.com>
2026-03-17 22:56:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,11 +21,14 @@ bot.action('services', (ctx) => {
   ctx.editMessageText("📋 Available Services:", {
     reply_markup: {
       inline_keyboard: [
-  [{ text: "1 Hour video call ₹350", callback_data: "buy_1" }],
-  [{ text: "1 hour sx chat ₹150", callback_data: "buy_2" }],
-  [{ text: "N*de photos ₹50", callback_data: "buy_3" }],
-  [{ text: "⬅ Back", callback_data: "menu" }]
-]
+        [{ text: "1 Hour video call ₹350", callback_data: "buy_1" }],
+        [{ text: "1 hour sx chat ₹150", callback_data: "buy_2" }],
+        [{ text: "N*de photos ₹50", callback_data: "buy_3" }],
+        [{ text: "⬅ Back", callback_data: "menu" }]
+      ]
+    }
+  });
+});
 
 // BUY
 bot.action(/buy_(.+)/, (ctx) => {
